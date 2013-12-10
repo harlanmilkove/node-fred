a4ddeed1324cd4747095122e0495da5dfb042def
Guillermo A. Sanchez <memo.sanchez@gmail.com>
2013-12-10 14:50:31 -0500

    Fix possible error when an http error occurs
    * Removed possible bug where an undefined body was being parsed
    * Added parsing of JSON body using request library
    * Added test for checking bodies are null when an error occurs

--- test/test-category.js
+++ test/test-category.js
@@ -1,4 +1,5 @@
 var fred = require('../lib/fred')('f75edf02cf8bbcbf62a7cd8f88488b31')
+var should = require('should')
 
 describe('Categories', function() {
   describe('/fred/category', function() {
@@ -9,6 +10,16 @@ describe('Categories', function() {
         category.should.have.property('parent_id')
 
         done(err)
+      })
+    })
+
+    it('Errors on a non-existant category', function(done) {
+      fred.category(123456789, function(err, category) {
+        should.exist(err)
+        err.should.be.an.instanceof(Error)
+        should.not.exist(category)
+
+        done()
       })
     })
   })
@@ -23,7 +34,7 @@ describe('Categories', function() {
   })
 
   describe('/fred/category/related', function() {
-    it('Returns children', function(done) {
+    it('Returns related categories', function(done) {
       fred.category.related(13, function(err, relatedCategories) {
         relatedCategories.should.be.an.instanceof(Array)
         done(err)
